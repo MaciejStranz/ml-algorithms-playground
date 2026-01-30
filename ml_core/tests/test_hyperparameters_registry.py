7dad0c6667e265fc5226fc078fa2591f7d1ef046
Maciej <stranz.maciej@gmail.com>
2026-01-30 18:11:14 +0100

    added hyperparameter filtering test

--- ml_core/tests/test_hyperparameters_registry.py
+++ ml_core/tests/test_hyperparameters_registry.py
@@ -1,7 +1,7 @@
 import pytest
 
-from ml_core.common.types import ParamType
-from ml_core.algorithms.hparam_specs import ALGORITHM_HPARAMS  # <- dostosuj import do nazwy pliku
+from ml_core.common.types import ParamType, TaskType
+from ml_core.algorithms.hparam_specs import ALGORITHM_HPARAMS, get_hyperparameter_specs
 from ml_core.common.hyperparameters import validate_value_against_spec
 
 
@@ -33,3 +33,15 @@ def test_registry_specs_are_consistent():
                 # Note: for ParamType.INT, a float default like 1.0 will pass "number" check,
                 # which you may or may not want. If you want strict int, enforce separately.
                 validate_value_against_spec(s, s.default)
+
+def test_svm_epsilon_is_only_for_regression():
+    """
+    Contract test: SVM 'epsilon' is an SVR-only hyperparameter (regression).
+    """
+    regression_specs = get_hyperparameter_specs("svm", TaskType.REGRESSION)
+    binary_specs = get_hyperparameter_specs("svm", TaskType.BINARY)
+    multiclass_specs = get_hyperparameter_specs("svm", TaskType.MULTICLASS)
+
+    assert any(s.name == "epsilon" for s in regression_specs)
+    assert all(s.name != "epsilon" for s in binary_specs)
+    assert all(s.name != "epsilon" for s in multiclass_specs)
